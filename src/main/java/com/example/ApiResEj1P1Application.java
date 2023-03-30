b6dc015915289b028a2b47dc8f31d4382640f389
belco <bcollado91@hotmail.com>
2023-03-30 14:52:17 +0200

    inicio

---
+++ src/main/java/com/example/ApiResEj1P1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResEj1P1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResEj1P1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiResEj1P1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResEj1P1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

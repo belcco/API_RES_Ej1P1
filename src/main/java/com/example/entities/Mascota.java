f78e86a3171f2daa2f221a231d100eee5ae070a5
belco <bcollado91@hotmail.com>
2023-03-30 17:40:17 +0200

    Entidades terminadas

---
+++ src/main/java/com/example/entities/Mascota.java
@@ -0,0 +1,52 @@
+package com.example.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "mascotas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Mascota implements Serializable    {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty(message = "El nombre no puede estar vacío")
+    @Size(min = 2, max = 25, message = "El nombre tiene que estar entre 2 y 25 caracteres")
+    private String nombre;
+
+    private String raza;
+
+    private Genero genero;
+
+    public enum Genero {
+        MACHO, HEMBRA,
+    }
+
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private Cliente cliente;
+
+    
+}
